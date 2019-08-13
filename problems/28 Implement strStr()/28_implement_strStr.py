03507406c6416ef6257f2b708752e7b21bb84679
Yuhjiang <jiang.yuhao0809@gmail.com>
2019-08-13 21:35:37 +0800

    28 Implement_strStr

--- problems/28 Implement strStr()/28_implement_strStr.py
+++ problems/28 Implement strStr()/28_implement_strStr.py
@@ -1,3 +1,23 @@
 """
-123124
+在haystack里寻找needle
 """
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        left, right = 0, len(needle) - 1
+        res = -1
+        while right < len(haystack):
+            if haystack[left:right+1] == needle:
+                res = left
+                break
+            else:
+                left += 1
+                right += 1
+
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.strStr('aaaaa', 'bba'))
