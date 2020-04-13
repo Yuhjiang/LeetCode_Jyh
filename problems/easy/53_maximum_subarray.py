ed92390865aca8add85faa23826d0b7d41a10d0c
jiangyuhao <jiang.yuhao0809@gmail.com>
2020-04-13 15:57:12 +0800

    21合并链表 & 53 最大子序列 & 70 爬楼梯

--- problems/easy/53_maximum_subarray.py
+++ problems/easy/53_maximum_subarray.py
@@ -17,7 +17,36 @@ class Solution:
         return current_max
 
 
+class DivideSolution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self._max_sub_array(nums, 0, len(nums) - 1)
+
+    def _max_sub_array(self, nums, left, right):
+        if left == right:
+            return nums[left]
+        mid = (left + right) // 2
+        return max(self._max_sub_array(nums, left, mid),
+                   self._max_sub_array(nums, mid+1, right),
+                   self._max_mid_array(nums, left, mid, right))
+
+    def _max_mid_array(self, nums, left, mid, right):
+        left_max_sum = 0
+        left_temp = 0
+        for i in range(mid - 1, left - 1, -1):
+            left_temp += nums[i]
+            left_max_sum = max(left_max_sum, left_temp)
+
+        right_max_sum = 0
+        right_temp = 0
+        for j in range(mid + 1, right + 1):
+            right_temp += nums[j]
+            right_max_sum = max(right_max_sum, right_temp)
+
+        return left_max_sum + nums[mid] + right_max_sum
+
+
 if __name__ == '__main__':
-    s = Solution()
+    s = DivideSolution()
     print(s.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-    print(s.maxSubArray([-2, -3, -1]))
+    # print(s.maxSubArray([-2, -3, -1]))
+    print(s.maxSubArray([1, 2]))
---
+++ problems/easy/70_climb_stairs.py
@@ -0,0 +1,24 @@
+"""
+这道题很直观的就是动态规划问题，i为要爬的阶数
+dp[i] = dp[i-1] + 1
+dp[i] = dp[i-2 + 2
+"""
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0] = 1
+        for i in range(1, n+1):
+            if i - 1 >= 0:
+                dp[i] += dp[i-1]
+            if i - 2 >= 0:
+                dp[i] += dp[i-2]
+        return dp[n]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.climbStairs(3))
+    print(s.climbStairs(2))
+    print(s.climbStairs(1))
