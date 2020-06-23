50c6b31de23672b1b8a586e7600c2212c3b63ee8
jiangyuhao <jiang.yuhao0809@gmail.com>
2020-06-23 15:44:30 +0800

    67 增加二进制加法新解法

--- problems/easy/67_add_binary.py
+++ problems/easy/67_add_binary.py
@@ -34,6 +34,44 @@ class Solution:
         return res
 
 
+class NewSolution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        if len(a) > len(b):
+            a, b = b, a
+        a = a[::-1]
+        b = b[::-1]
+
+        plus = 0
+        for i in range(len(a)):
+            num, plus = self.add(a[i], b[i], plus)
+            res.append(num)
+
+        for j in range(len(a), len(b)):
+            num, plus = self.add('0', b[j], plus)
+            res.append(num)
+        if plus:
+            res.append(plus)
+
+        return ''.join([str(s) for s in res[::-1]])
+
+    def add(self, a: str, b: str, plus: int):
+        if plus:
+            if a == '1' and b == '1':
+                return 1, 1
+            elif a == '0' and b == '0':
+                return 1, 0
+            else:
+                return 0, 1
+        else:
+            if a == '1' and b == '1':
+                return 0, 1
+            elif a == '0' and b == '0':
+                return 0, 0
+            else:
+                return 1, 0
+
+
 if __name__ == '__main__':
-    s = Solution()
-    print(s.addBinary('1010', '1011'))
+    s = NewSolution()
+    print(s.addBinary('11', '1'))
