c349c0c428ff9699e6865152d7fc7f99be186f3a
jiangyuhao <jiang.yuhao0809@gmail.com>
2020-06-25 15:06:18 +0800

    406 按身高排序新解法

--- problems/midium/406_queue_reconstruction_by_height.py
+++ problems/midium/406_queue_reconstruction_by_height.py
@@ -21,6 +21,17 @@ class Solution:
         return queue
 
 
+class NewSolution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+
+        result = []
+        for i in range(len(people)):
+            result.insert(people[i][1], people[i])
+
+        return result
+
+
 if __name__ == '__main__':
-    # print(Solution().reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]))
-    print(Solution().reconstructQueue([[7, 0], [5, 1]]))
+    print(NewSolution().reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]))
+    # print(Solution().reconstructQueue([[7, 0], [5, 1]]))
